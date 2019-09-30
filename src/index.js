de61bb66afcc1580253075b59343c11d3dd2771f
Raimedas Simutis <raimedas.simutis@gmail.com>
2019-10-01 00:25:10 +0300

    resizible sidebar added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-import './assets/style/app.scss'
+$(function() {
+
+    $('[data-toggle]').on( "click", function(e) {
+        e.preventDefault();
+
+        let element = $(this).data('toggle');
+
+        $('[data-' + element + ']')
+        .toggleClass(element + '-hide');
+
+        //$(`[data-${$(this).data('toggle')}]`)
+        //.toggleClass(${$(this).data('toggle')} );
+    });
+    
+
+
+
+
+
+});
+
+
+  
